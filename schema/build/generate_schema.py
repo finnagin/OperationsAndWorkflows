682bfaa309ff947c0fb760b6fbbdd09c6d7b81ac
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-02-19 15:35:53 -0500

    Update operation schema generation

--- schema/build/generate_schema.py
+++ schema/build/generate_schema.py
@@ -58,6 +58,8 @@ if __name__ == "__main__":
         for filename in glob.glob(str(OPERATIONS_DIR / "*.yml"))
     ]
     operation = {
+        "$schema": "https://json-schema.org/draft-07/schema",
+        "$id": "http://standards.ncats.io/operation.json",
         "anyOf": [
             {
                 "$ref": f"#/definitions/{op_to_component(op)}"
@@ -69,5 +71,5 @@ if __name__ == "__main__":
             for op in operations
         },
     }
-    with open(SCHEMA_DIR / "json_schema.json", "w") as stream:
+    with open(SCHEMA_DIR / "operation.json", "w") as stream:
         json.dump(operation, stream, indent=4)
