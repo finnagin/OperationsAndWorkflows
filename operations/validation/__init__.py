ca954bf68639c450878570bf7eceb5d77a9bf56f
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-04-28 14:58:48 -0400

    Set up automated validation

--- operations/validation/__init__.py
+++ operations/validation/__init__.py
@@ -9,10 +9,13 @@ from yaml.loader import SafeLoader
 THIS_DIR = Path(__file__).parent
 OPS_DIR = THIS_DIR.parent
 
-with open(THIS_DIR / "schema.json", "r") as stream:
-    meta_schema = json.load(stream)
 
-for filename in OPS_DIR.glob("*.yml"):
-    with open(filename, "r") as stream:
-        op_spec = yaml.load(stream, Loader=SafeLoader)
-    jsonschema.validate(op_spec, meta_schema)
+def validate_operations():
+    """Validate operations against operation specification schema."""
+    with open(THIS_DIR / "schema.json", "r") as stream:
+        meta_schema = json.load(stream)
+
+    for filename in OPS_DIR.glob("*.yml"):
+        with open(filename, "r") as stream:
+            op_spec = yaml.load(stream, Loader=SafeLoader)
+        jsonschema.validate(op_spec, meta_schema)
---
+++ tests/test_validate.py
@@ -0,0 +1,7 @@
+"""Test that all operations are valid."""
+from operations.validation import validate_operations
+
+
+def test_validate_ops():
+    """Test that all operations are valid."""
+    validate_operations()
