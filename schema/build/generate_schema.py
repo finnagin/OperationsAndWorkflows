9addb73b4daddb8345fdf2388a0c9545fda118b0
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-10-12 13:11:31 -0400

    Add version-handling to `generate_schema()`

--- schema/build/generate_schema.py
+++ schema/build/generate_schema.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Generate JSON Schema for operations."""
+import argparse
 import glob
 import json
 from pathlib import Path
@@ -53,14 +54,31 @@ def op_to_component(op):
     return "Operation" + snakecase_to_pascalcase(op)
 
 
+def get_next_version():
+    """Get next version.
+    
+    Get the current version from the workflow $id, and increment the minor version number.
+    """
+    with open(SCHEMA_DIR / "workflow.json", "r") as stream:
+        workflow = json.load(stream)
+    current_version = workflow["$id"].split("/")[-2]
+    major, minor, patch = current_version.split(".")
+    minor = str(int(minor) + 1)
+    return f"{major}.{minor}.{patch}"
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", default=get_next_version())
+    args = parser.parse_args()
+    version = args.version
     operations = sorted([    
         Path(filename).stem
         for filename in glob.glob(str(OPERATIONS_DIR / "*.yml"))
     ])
     operation = {
         "$schema": "https://json-schema.org/draft-07/schema",
-        "$id": "http://standards.ncats.io/operation/1.0.0/schema",
+        "$id": f"http://standards.ncats.io/operation/{version}/schema",
         "anyOf": [
             {
                 "$ref": f"#/$defs/{op_to_component(op)}"
@@ -74,3 +92,13 @@ if __name__ == "__main__":
     }
     with open(SCHEMA_DIR / "operation.json", "w") as stream:
         json.dump(operation, stream, indent=4)
+    workflow = {
+        "$schema": "https://json-schema.org/draft-07/schema",
+        "$id": f"http://standards.ncats.io/workflow/{version}/schema",
+        "type": "array",
+        "items": {
+            "$ref": f"http://standards.ncats.io/operation/{version}/schema"
+        }
+    }
+    with open(SCHEMA_DIR / "workflow.json", "w") as stream:
+        json.dump(workflow, stream, indent=4)
